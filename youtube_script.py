c78e4f1e2e48c98d1b0d4f309b846ea983634c5c
FrenchCommando <>
2020-11-17 16:53:10 -0500

    youtube script

--- youtube_script.py
+++ youtube_script.py
@@ -2,27 +2,28 @@ from pytube import YouTube
 
 
 link = "https://www.youtube.com/watch?v=9PgtyeugRvU"
+link = "https://www.youtube.com/watch?v=9HGh8f7Twn8"
 output_folder = "output"
 
 
 yt = YouTube(link)
-stream = yt.streams.all()
-for s in stream:
-    print(s)
-    s.download(output_path=output_folder, filename=f"{s}")
+# stream = yt.streams.all()
+# for s in stream:
+#     print(s)
+#     s.download(output_path=output_folder, filename=f"{s}")
 
 
-yt.streams\
-    .filter(progressive=True, file_extension='mp4')\
-    .order_by('resolution')\
-    .first()\
-    .download(output_path=output_folder, filename_prefix="progressive")
-
-yt.streams\
-    .filter(progressive=False, file_extension='webm')\
-    .order_by('resolution')\
-    .last()\
-    .download(output_path=output_folder, filename_prefix="video")
+# yt.streams\
+#     .filter(progressive=True, file_extension='mp4')\
+#     .order_by('resolution')\
+#     .first()\
+#     .download(output_path=output_folder, filename_prefix="progressive")
+#
+# yt.streams\
+#     .filter(progressive=False, file_extension='webm')\
+#     .order_by('resolution')\
+#     .last()\
+#     .download(output_path=output_folder, filename_prefix="video")
 
 out = yt.streams\
     .filter(progressive=False, file_extension='webm')\
